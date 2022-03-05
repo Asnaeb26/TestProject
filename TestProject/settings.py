db18aa852e1267c9fa3fa6db0354d95abd0face4
ilya <vp-11@mail.ru>
2022-03-05 12:40:28 +0300

    Вынес логику из MessageViewSet - create() в отдельный класс MessageAction

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'firstapp.apps.FirstappConfig',
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
